702d0fab691b3356957fff2630cbd0c1a444f0ff
Mohmn <naqashmohmin1@gmail.com>
2024-06-07 02:26:41 +0530

    feat: add k8s local setup

--- apps/like/src/main.ts
+++ apps/like/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3010;
+  console.log('env process', process.env.DATABASE_URL, process.env.environment)
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}`
